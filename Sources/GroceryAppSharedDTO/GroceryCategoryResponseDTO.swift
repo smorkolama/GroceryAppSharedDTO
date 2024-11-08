06a16acd74391ea4c62281872f90d9ef2fd8b792
Van Den Hout Benjamin, SUB-SDC-NL-DOS-OCD <Benjamin.VanDenHout@swisscom.com>
2024-11-08 16:25:34 +0100

    Should be GroceryItemResponseDTO

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -12,9 +12,9 @@ public struct GroceryCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let colorCode: String
-    public let items: [GroceryCategoryResponseDTO]
+    public let items: [GroceryItemResponseDTO]
 
-    public init(id: UUID, title: String, colorCode: String, items: [GroceryCategoryResponseDTO] = []) {
+    public init(id: UUID, title: String, colorCode: String, items: [GroceryItemResponseDTO] = []) {
         self.id = id
         self.title = title
         self.colorCode = colorCode
